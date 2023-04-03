c06ce9a5312aaccd49d5d56c294004e90226140f
arjun.airtory <arjun12345krishna@gmail.com>
2023-04-03 19:50:11 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server started on PORT 3000!');
+});
