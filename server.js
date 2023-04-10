bb7bf4777d094e770cd19559e6bac825e1d6949f
arjun.airtory <arjun12345krishna@gmail.com>
2023-04-10 22:18:49 +0530

    Adding CORS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors');
 
 var database = require('./config/database.js');
 database.connect();
@@ -13,9 +14,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var userRoutes = require('./routes/user');
 var surveyRoutes = require('./routes/survey.js');   
 
+app.use(cors());
 app.use('/user', userRoutes);
 app.use('/survey', surveyRoutes);
 
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 
 app.listen(3000, () => {
